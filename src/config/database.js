16d00127b8c8d27e13a5f0438e1b268775dbf459
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-07 17:52:27 -0300

    Adds custom error validation messages

--- src/config/database.js
+++ src/config/database.js
@@ -2,3 +2,8 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise // forces to use the mongoose to use the Node global Promise API
 
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "'{PATH}' is mandatory."
+mongoose.Error.messages.Number.min = "'{VALUE}' is less than the minimum threshold of '{MIN}'."
+mongoose.Error.messages.Number.max = "'{VALUE}' is greater the the maximum threshold of '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' is not a valid attribute for '{PATH}'."
