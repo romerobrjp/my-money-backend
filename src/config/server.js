3950825a5e556072d9cbf9318e197c3b2969454c
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-06 22:47:41 -0300

    Setups the server

---
+++ src/config/server.js
@@ -0,0 +1,12 @@
+const SERVER_PORT = 3003
+
+const bodyParser = require('body-parser') // middleware that helps to parse the http body
+const express = require('express') // singleton instance
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(SERVER_PORT, function() {
+  console.log(`My Money App API running on port ${SERVER_PORT}`)
+})
