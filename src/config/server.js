5a136f210520cebb519cc7ca803b57ced7092fd1
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-07 22:09:36 -0300

    Adds CORS middleware.

--- src/config/server.js
+++ src/config/server.js
@@ -2,10 +2,12 @@ const SERVER_PORT = 3003
 
 const bodyParser = require('body-parser') // middleware that helps to parse the http body
 const express = require('express') // singleton instance
+const allowCors = require('./cors')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(SERVER_PORT, function() {
   console.log(`My Money App API running on port ${SERVER_PORT}`)
