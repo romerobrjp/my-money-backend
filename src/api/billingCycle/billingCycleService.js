32cdb0e892c6648f513299a9c0315a5364759796
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-07 21:54:01 -0300

    Implements the summary()

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -13,4 +13,36 @@ BillingCycle.route('count', (req, res, next) => {
   })
 })
 
+BillingCycle.route('summary', (req, res, next) => {
+  BillingCycle.aggregate([
+    {
+      $project: {
+        credit: { $sum: "$credits.value"},
+        debt: { $sum: "$debts.value" }
+      }
+    },
+    {
+      $group: { 
+        _id: null,
+        credit_total: { $sum: "$credit" },
+        debt_total: { $sum: "$debt" },
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        credit_total: 1,
+        debt_total: 1
+      }
+    }],
+    (error, result) => {
+      if (!!error) {
+        res.status(500).json({ errors: [error] })
+      } else {
+        res.json(result[0] || { credit_total: 0, debt_total: 0 })
+      }
+    }
+  )
+})
+
 module.exports = BillingCycle
