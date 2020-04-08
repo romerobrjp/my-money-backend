6889d9e239576be8a8ae08da1f3a37516e24f3d1
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-07 21:17:48 -0300

    Implements count() in the Service.

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,6 +1,16 @@
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true, runValidators: true}) // it forces mongo to return updated object, and to run the validations
+BillingCycle.updateOptions({new: true, runValidators: true}) // it forces mongo to return the updated object, and to run the validations
+
+BillingCycle.route('count', (req, res, next) => {
+  BillingCycle.count((error, countValue) => {
+    if (!!error) {
+      res.status(500).json({ errors: [error] })
+    } else {
+      res.json({ value: countValue })
+    }
+  })
+})
 
 module.exports = BillingCycle
