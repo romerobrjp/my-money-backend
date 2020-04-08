18cc116bf514cdefc214d1decaad2cd837331036
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-07 22:16:44 -0300

    Uses express-query-int middleware

--- src/config/server.js
+++ src/config/server.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser') // middleware that helps to parse the 
 const express = require('express') // singleton instance
 const allowCors = require('./cors')
 const server = express()
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(SERVER_PORT, function() {
   console.log(`My Money App API running on port ${SERVER_PORT}`)
