f938a48f7f0c4ca2e4df9a00213bf1f1b6615988
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-29 23:05:38 -0300

    Fixes typo

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -2,20 +2,20 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
-  name: { type: String, require: true },
-  value: { type: Number, require: true, min: 0 },
+  name: { type: String, required: true },
+  value: { type: Number, required: true, min: 0 },
 })
 
 const debtSchema = new mongoose.Schema({
-  name: { type: String, require: true },
-  value: { type: Number, require: true, min: 0 },
-  status: { type: String, require: false, uppercase: true, enum: ['PAID', 'PENDING', 'SCHEDULED'] }
+  name: { type: String, required: true },
+  value: { type: Number, required: true, min: 0 },
+  status: { type: String, required: false, uppercase: true, enum: ['PAID', 'PENDING', 'SCHEDULED'] }
 })
 
 const billingCycleSchema = new mongoose.Schema({
-  name: { type: String, require: true },
-  month: { type: Number, require: true, min: 1, max: 12 },
-  year: { type: Number, require: true, min: 1970, max: 2100 },
+  name: { type: String, required: true },
+  month: { type: Number, required: true, min: 1, max: 12 },
+  year: { type: Number, required: true, min: 1970, max: 2100 },
   credits: [creditSchema],
   debts: [debtSchema]
 })
